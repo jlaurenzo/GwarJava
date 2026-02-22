6892f8f8c4791752215c65efb54396a34be79da4
jlaurenzo <pazlaurencejohn@gmail.com>
2026-02-22 16:33:56 +0800

    Added ability to write to the accounts.txt.

--- src/system/CreateAccount.java
+++ src/system/CreateAccount.java
@@ -1,19 +1,19 @@
 package system;
 
 import java.util.*;
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class CreateAccount {
 
-    String newAccountName;
-    String newAccountPassword;
+    String newAccountName = "";
+    String newAccountPassword = "";
     String confirmAccountPassword;
     public void createNewAccount() {
 
         Scanner userInput = new Scanner(System.in);
 
-        System.out.println("Create account page");
+        System.out.println("---CREATE NEW ACCOUNT---");
         System.out.print("Enter Account Name: ");
         newAccountName = userInput.nextLine();
         System.out.print("Enter Account Password: ");
@@ -22,7 +22,18 @@ public class CreateAccount {
         confirmAccountPassword = userInput.nextLine();
 
         if (newAccountPassword.equals(confirmAccountPassword)) {
-            System.out.println("Account created");
+
+            try {
+                FileWriter databaseWriter = new FileWriter("accounts.txt", true);
+                databaseWriter.write(newAccountName + " , " + newAccountPassword + "\n");
+                databaseWriter.close();
+            } catch (IOException e) {
+                System.out.println("An errror occured!");
+            }
+            System.out.println("Account created\n");
+        } else {
+            System.out.println("Password does not match, try again! \n");
+            createNewAccount();
         }
 
     }
